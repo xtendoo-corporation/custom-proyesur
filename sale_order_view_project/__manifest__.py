223f43825ca1f48289e001ed862e8df857997d14
NotJaime13 <jaimemillan13@hotmail.com>
2023-03-30 11:50:12 +0200

    Project visible in Tree and in lines

---
+++ sale_order_view_project/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2023 Xtendoo
+
+{
+    "name": "Sale Order View Project",
+    "summary": """
+        Sale Order View Project""",
+    "version": "15.0",
+    "depends": [
+        "sale_management",
+        "contacts",
+        "purchase",
+        "sale_project",
+    ],
+    "maintainers": ["NotJaime13"],
+    "author": "Xtendoo",
+    "license": "AGPL-3",
+    "data": [
+        "views/sale_order.xml",
+    ],
+    "application": True,
+    "installable": True,
+    "auto_install": True,
+}
+
