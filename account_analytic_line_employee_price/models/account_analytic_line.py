613647d350328f59ea7c1704c99e6d69e2efb579
dacrumau <dacrumau@gmail.com>
2023-11-13 14:17:16 +0100

    precio

--- account_analytic_line_employee_price/models/account_analytic_line.py
+++ account_analytic_line_employee_price/models/account_analytic_line.py
@@ -7,29 +7,24 @@ from odoo import fields, models, _
 class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
 
+
     def _compute_employee_timesheet_cost_total(self):
         for line in self:
             line.employee_timesheet_cost_total = line.unit_amount * line.employee_timesheet_cost
 
+    def _compute_set_employee_timesheet_cost(self):
+        for line in self:
+            if line.employee_timesheet_cost == 0.0:
+                line.employee_timesheet_cost = line.employee_id.timesheet_cost
+
     employee_timesheet_cost = fields.Monetary(
         string="Employee timesheet cost",
-        related="employee_id.timesheet_cost",
-        currency_field='currency_id',
+        compute='_compute_set_employee_timesheet_cost',
         groups="hr.group_hr_user",
     )
 
     employee_timesheet_cost_total = fields.Monetary(
         string="Employee timesheet cost total",
         compute='_compute_employee_timesheet_cost_total',
-        currency_field='currency_id',
         groups="hr.group_hr_user",
     )
-
-    currency_id = fields.Many2one(
-        'res.currency',
-        related='company_id.currency_id',
-        readonly=True
-    )
-
-
-
