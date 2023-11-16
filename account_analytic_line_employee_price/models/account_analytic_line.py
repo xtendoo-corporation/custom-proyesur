30af3241c9807eed347dc56f5be0ee64395eae4a
Manuel Calero <manuelcalerosolis@gmail.com>
2023-11-16 13:25:14 +0100

    timesheet cost store default

--- account_analytic_line_employee_price/models/account_analytic_line.py
+++ account_analytic_line_employee_price/models/account_analytic_line.py
@@ -22,7 +22,16 @@ class AccountAnalyticLine(models.Model):
     employee_timesheet_cost = fields.Monetary(
         string="Employee timesheet cost",
         groups="hr.group_hr_user",
+        default=lambda self: self._get_default_employee_timesheet_cost(),
     )
+
+    @api.model
+    def _get_default_employee_timesheet_cost(self):
+        print("*" * 100)
+        print("line.employee_id", self.employee_id)
+        print("line.employee_id.timesheet_cost", self.employee_id.timesheet_cost)
+        print("*" * 100)
+        return self.employee_id.timesheet_cost
 
     employee_timesheet_cost_total = fields.Monetary(
         string="Employee timesheet cost total",
