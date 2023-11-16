3060fff8cf832720264cd03e9b521350bbc9eead
Manuel Calero <manuelcalerosolis@gmail.com>
2023-11-16 11:24:52 +0100

    timesheet cost store

--- account_analytic_line_employee_price/models/account_analytic_line.py
+++ account_analytic_line_employee_price/models/account_analytic_line.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Camilo <Xtendoo, https://xtendoo.es/>.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models, _
+from odoo import api, fields, models, _
 
 
 class AccountAnalyticLine(models.Model):
@@ -10,16 +10,17 @@ class AccountAnalyticLine(models.Model):
 
     def _compute_employee_timesheet_cost_total(self):
         for line in self:
-            line.employee_timesheet_cost_total = line.unit_amount * line.employee_timesheet_cost
-
-    def _compute_set_employee_timesheet_cost(self):
-        for line in self:
             if line.employee_timesheet_cost == 0.0:
                 line.employee_timesheet_cost = line.employee_id.timesheet_cost
+            line.employee_timesheet_cost_total = line.unit_amount * line.employee_timesheet_cost
+
+    @api.onchange('employee_id')
+    def _onchange_employee_id(self):
+        for line in self:
+            line.employee_timesheet_cost = line.employee_id.timesheet_cost
 
     employee_timesheet_cost = fields.Monetary(
         string="Employee timesheet cost",
-        compute='_compute_set_employee_timesheet_cost',
         groups="hr.group_hr_user",
     )
 
@@ -27,4 +28,5 @@ class AccountAnalyticLine(models.Model):
         string="Employee timesheet cost total",
         compute='_compute_employee_timesheet_cost_total',
         groups="hr.group_hr_user",
+        readonly=True,
     )
